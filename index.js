864922baae7265a99a933ec437d32fca2bdbd41c
Bryce McDonnell <bryce@bridgetownint.com>
2014-09-13 16:28:47 -0400

    it's pretty much working

--- index.js
+++ index.js
@@ -56,3 +56,102 @@ exampleApp.controller('cartCtrl', function($scope, CartItemsFactory) {
   $scope.cart = CartItemsFactory;
 });
 
+
+exampleApp.factory("BikeStationCensus", function($http){
+
+  return {
+    allBikeStations : function(){
+      var stations = {};
+      $http.get("http://www.citibikenyc.com/stations/json").success(function(data){
+        console.log(data);
+        stations = data;
+      });
+      return stations;
+      return {
+            "executionTime": "2014-09-13 01:41:01 PM",
+            "stationBeanList": [
+                  {
+              "id": 72,
+              "stationName": "W 52 St & 11 Ave",
+              "availableDocks": 21,
+              "totalDocks": 39,
+              "latitude": 40.76727216,
+              "longitude": -73.99392888,
+              "statusValue": "In Service",
+              "statusKey": 1,
+              "availableBikes": 18,
+              "stAddress1": "W 52 St & 11 Ave",
+              "stAddress2": "",
+              "city": "",
+              "postalCode": "",
+              "location": "",
+              "altitude": "",
+              "testStation": false,
+              "lastCommunicationTime": null,
+              "landMark": ""
+            },
+            {
+              "id": 79,
+              "stationName": "Franklin St & W Broadway",
+              "availableDocks": 29,
+              "totalDocks": 33,
+              "latitude": 40.71911552,
+              "longitude": -74.00666661,
+              "statusValue": "In Service",
+              "statusKey": 1,
+              "availableBikes": 4,
+              "stAddress1": "Franklin St & W Broadway",
+              "stAddress2": "",
+              "city": "",
+              "postalCode": "",
+              "location": "",
+              "altitude": "",
+              "testStation": false,
+              "lastCommunicationTime": null,
+              "landMark": ""
+            }]
+      }
+    }
+  };
+});
+
+exampleApp.factory("WatchedBikeStations", function(BikeStationCensus) {
+
+  var watchList = [72];
+
+  return {
+    watchedIdList : watchList,
+    stationCount: function() {
+      return this.watchedIdList.length;
+    },
+    watchedStations : function() {
+      var stationObjects = [];
+      angular.forEach(this.watchedIdList, function(ind, val) {
+        var theStation = _.find(BikeStationCensus.allBikeStations().stationBeanList, function(ele) {
+          return ele.id == ind
+        });
+
+        if(theStation) {
+          stationObjects.push(theStation);
+        };
+      }, stationObjects);
+      return stationObjects;
+    }
+  }
+});
+
+exampleApp.controller('bikeFeedCtrl', function($scope, BikeStationCensus, WatchedBikeStations) {
+  $scope.allBikeStations = BikeStationCensus.allBikeStations();
+  $scope.watchedBikeStations = WatchedBikeStations.watchedIdList;
+
+  $scope.addToWatchList = function(id) {
+    $scope.watchedBikeStations.push(id);
+  };
+});
+
+exampleApp.controller('trackingCtrl', function($scope, BikeStationCensus, WatchedBikeStations) {
+  $scope.watchedStationIds = WatchedBikeStations.watchedIdList;
+  $scope.watchFactory = WatchedBikeStations;
+  $scope.lastUpdated = BikeStationCensus.allBikeStations().executionTime;
+
+});
