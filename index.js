fd5d9b99e72e821f94a6e74b76361d53262f142a
Bryce McDonnell <bryce@bridgetownint.com>
2014-09-13 12:50:06 -0400

    displaying products and cart items

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-angular.module('exampleApp', [])
-.controller('exampleCtrl1', function() {
+var exampleApp = angular.module('exampleApp', []);
 
-})
-.controller('exampleCtrl2', function() {
+exampleApp.factory("CartItemsFactory", function(){
+  var cartItems = [
+    {name: 'Default Product', price: 12.99}
+  ];
+
+  return {
+    cartItems: cartItems
+  };
 
 });
+
+exampleApp.factory("AvailableProductsFactory", function(){
+  var allProducts = [
+    {name: 'Product 1', price: 12.99},
+    {name: 'Product 2', price: 22.99},
+    {name: 'Product 3', price: 32.99},
+    {name: 'Product 4', price: 42.99}
+  ];
+
+  return {
+    allProducts: allProducts
+  };
+
+});
+
+exampleApp.controller('productCtrl', function($scope, CartItemsFactory, AvailableProductsFactory) {
+  $scope.cartItems = CartItemsFactory.cartItems;
+  $scope.allProducts = AvailableProductsFactory.allProducts;
+});
+
+exampleApp.controller('cartCtrl', function($scope, CartItemsFactory) {
+  $scope.cartItems = CartItemsFactory.cartItems;
+});
+
